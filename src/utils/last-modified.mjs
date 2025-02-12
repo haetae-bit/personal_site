e82590323d37c4b33bc868933363dcc1cce466b1
haetae <hello@haetae.nya.pub>
2025-02-12 03:26:05 -0500

    add more styles and implement theme switching

--- src/utils/last-modified.mjs
+++ src/utils/last-modified.mjs
@@ -1,7 +1,7 @@
 import { statSync } from "fs";
 
 export function modifiedTime() {
-  return function (tree, file) {
+  return function (_tree, file) {
     const path = file.history[0];
     const result = statSync(path);
     file.data.astro.frontmatter.lastModified = result.mtime;
