5e9bbeae33bc5345d2b27ece5d2263e6ee316965
haetae <haetae@32bit.cafe>
2025-05-15 20:00:35 -0400

    use experimental fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,4 +11,144 @@ export default defineConfig({
     smartypants: false,
   },
   integrations: [alpinejs()],
+  experimental: {
+    fonts: [
+      {
+        provider: "local",
+        name: "Arial Pixel",
+        cssVariable: "--arial",
+        variants: [
+          {
+            weight: 400,
+            style: "normal",
+            src: ['./src/assets/fonts/pixearg.woff2']
+          },
+          {
+            weight: 700,
+            style: "normal",
+            src: ['./src/assets/fonts/pixeab.woff2']
+          },
+        ]
+      },
+      {
+        provider: "local",
+        name: "Departure Mono",
+        cssVariable: "--mono",
+        variants: [{
+          weight: 400,
+          style: "normal",
+          src: ['./src/assets/fonts/DepartureMono-Regular.woff2']
+        }]
+      },
+      {
+        provider: "local",
+        name: "Dotum 11",
+        cssVariable: "--dotum-11",
+        variants: [{
+          weight: 400,
+          style: "normal",
+          src: ["./src/assets/fonts/dotum-11.woff2"]
+        }]
+      },
+      {
+        provider: "local",
+        name: "Dotum 12",
+        cssVariable: "--dotum-12",
+        variants: [{
+          weight: 400,
+          style: "normal",
+          src: ["./src/assets/fonts/dotum-12.woff2"]
+        }]
+      },
+      {
+        provider: "local",
+        name: "DotumChe 11",
+        cssVariable: "--dotumche-11",
+        variants: [{
+          weight: 400,
+          style: "normal",
+          src: ["./src/assets/fonts/dotumche-11.woff2"]
+        }]
+      },
+      {
+        provider: "local",
+        name: "DotumChe 12",
+        cssVariable: "--dotumche-12",
+        variants: [{
+          weight: 400,
+          style: "normal",
+          src: ["./src/assets/fonts/dotumche-12.woff2"]
+        }]
+      },
+      {
+        provider: "local",
+        name: "Kiwi Soda",
+        cssVariable: "--kiwi",
+        variants: [{
+          weight: 400,
+          style: "normal",
+          src: ["./src/assets/fonts/KiwiSoda.woff2"]
+        }],
+      },
+      {
+        provider: "local",
+        name: "MLSS",
+        cssVariable: "--mlss",
+        variants: [{
+          weight: 400,
+          style: "normal",
+          src: ["./src/assets/fonts/mario-luigi-rpg-speech-text.woff2"]
+        }]
+      },
+      {
+        provider: "local",
+        name: "Redaction",
+        cssVariable: "--serif",
+        variants: [
+          {
+            weight: 400,
+            style: "normal",
+            src: ["./src/assets/fonts/Redaction_35-Regular.woff2"]
+          },
+          {
+            weight: 400,
+            style: "italic",
+            src: ["./src/assets/fonts/Redaction_35-Italic.woff2"]
+          },
+          {
+            weight: 700,
+            style: "normal",
+            src: ["./src/assets/fonts/Redaction_35-Bold.woff2"]
+          },
+        ]
+      },
+      {
+        provider: "local",
+        name: "sq",
+        cssVariable: "--sq",
+        variants: [
+          {
+            weight: 400,
+            style: "normal",
+            src: ["./src/assets/fonts/sq.woff2"]
+          },
+          {
+            weight: 700,
+            style: "normal",
+            src: ["./src/assets/fonts/sqb.woff2"]
+          }
+        ],
+      },
+      {
+        provider: "local",
+        name: "Wonder Mail",
+        cssVariable: "--wondermail",
+        variants: [{
+          weight: 400,
+          style: "normal",
+          src: ["./src/assets/fonts/wondermail.woff2"]
+        }]
+      },
+    ],
+  },
 });
--- src/pages/about.md
+++ src/pages/about.md
@@ -14,15 +14,15 @@ i used a bunch of assets that require attribution but i also wanted to link peop
 
 ## images
 - [Pikachu portrait](https://sprites.pmdcollab.org/#/0025?form=0) by SPIKE CHUNSOFT, curated by the PMD Collab project
-- [This about page's frame images](https://www.spriters-resource.com/ds_dsi/pokemonmysterydungeonexplorersoftimedarkness/sheet/5986/) by redblueyellow
+- [this about page's frame images](https://www.spriters-resource.com/ds_dsi/pokemonmysterydungeonexplorersoftimedarkness/sheet/5986/) by redblueyellow
 - the blog's frame images and additional assets (such as the mood emoticons) were graciously provided by AymShade on the MapleLegends forums! i can't directly link because i'm scared of a Certain Company™ being evil
-- [The guestbook's speech bubble images](https://www.spriters-resource.com/game_boy_advance/mlss/sheet/7573/) by MajinPiccolo
+- [the guestbook's speech bubble images](https://www.spriters-resource.com/game_boy_advance/mlss/sheet/7573/) by MajinPiccolo
 
 ## fonts
 - [Departure Mono](https://departuremono.com/) by [Helena Zhang](https://www.helenazhang.com/) is licensed under [OFL 1.1](https://www.tldrlegal.com/license/open-font-license-ofl-explained)
-- [Kiwi Soda](https://fontenddev.com/fonts/kiwi-soda/) from fontenddev.com is licensed under [CC by 4.0](https://creativecommons.org/licenses/by/4.0/)
+- [DotumChe](https://github.com/googlefonts/gulim) is licensed under [OFL 1.1](https://www.tldrlegal.com/license/open-font-license-ofl-explained)
+- [Kiwi Soda](https://fontenddev.com/fonts/kiwi-soda/) from [fontenddev.com](https://fontenddev.com/) is licensed under [CC by 4.0](https://creativecommons.org/licenses/by/4.0/)
 - [Mario & Luigi RPG Speech Text](https://fontstruct.com/fontstructions/show/1102228) by DarkMaxX is licensed under [CC by Share Alike 3.0](http://creativecommons.org/licenses/by-sa/3.0/)
-- [PixelMplus](https://itouhiro.hatenablog.com/entry/20130602/font) by itouhiro is licensed under [M+ font license](https://web.archive.org/web/20221024231351/http://mplus-fonts.osdn.jp/mplus-bitmap-fonts/#license)
 - [Redaction](https://www.redaction.us/) by [Forest Young](https://www.moma.org/interactives/exhibitions/2011/talktome/objects/140027/) and [Jeremy Mickel](https://mckltype.com/) is dual-licensed under [LGPL 2.1](https://www.tldrlegal.com/license/gnu-lesser-general-public-license-v2-1-lgpl-2-1) and [OFL 1.1](https://www.tldrlegal.com/license/open-font-license-ofl-explained)
 - [sq](https://github.com/leahneukirchen/sq) by [Leah Neukirchen](https://leahneukirchen.org/) is in public domain
 - [Wonder Mail](https://www.dafont.com/wonder-mail.font) by ShinxHijinx is free for personal use (as far as i know at the time of this writing: April 14, 2025)
