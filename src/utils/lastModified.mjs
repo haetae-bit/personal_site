e56feaf1d0f505edf0b5522e144e8ba821aed754
haetae <haetae@32bit.cafe>
2025-08-15 21:47:41 -0400

    last modified is flaky but. whatever

--- src/utils/lastModified.mjs
+++ src/utils/lastModified.mjs
@@ -3,11 +3,13 @@ import { statSync } from "fs";
 export function modifiedTime() {
   return function (_tree, file) {
     const path = file.history[0];
-    try {
-      const result = statSync(path);
-      file.data.astro.frontmatter.lastModified = result.mtime;
-    } catch (error) {
-      return;
-    }    
+    const result = statSync(path);
+    file.data.astro.frontmatter.lastModified = result.mtime;
+    // try {
+    //   const result = statSync(path);
+    //   file.data.astro.frontmatter.lastModified = result.mtime;
+    // } catch (error) {
+    //   return;
+    // }
   }
 }
